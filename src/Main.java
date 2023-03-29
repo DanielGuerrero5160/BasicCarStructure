baa23ab1f572f7eb470e0cdbdddcf61d8039ae90
DanielGuerrero5160 <daniel.guerrero5160@gmail.com>
2023-03-29 19:17:17 -0400

    The basic structure of the car is ready

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+       BasicStructure bs=new BasicStructure();
+       bs.color="blue";
+       bs.model="M3";
+       bs.power="420HP";
+       
+    }
+}
